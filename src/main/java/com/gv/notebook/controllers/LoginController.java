2dc717222e2dc0e2f4e82ddbf1c263ba032f0ef1
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-02-22 13:14:39 +0300

    update classes LoginController, UserValidator

--- src/main/java/com/gv/notebook/controllers/LoginController.java
+++ src/main/java/com/gv/notebook/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.gv.notebook.controllers;
 
 import com.gv.notebook.logic.UserValidator;
+import com.gv.notebook.models.User;
 import com.gv.notebook.settings.ConfigurationManager;
 
 import javax.servlet.RequestDispatcher;
@@ -8,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LoginController extends HttpServlet {
@@ -16,6 +18,7 @@ public class LoginController extends HttpServlet {
     private final static String PASSWORD_PARAMETER = "password";
     private final static String MAIN_PAGE = "path.page.main";
     private final static String ERROR_PAGE = "path.page.error";
+    private final static String ATTRIBUTE_USER = "user";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -26,11 +29,18 @@ public class LoginController extends HttpServlet {
     }
 
     private String processRequest(HttpServletRequest request, HttpServletResponse response){
-        String pagePath = ConfigurationManager.getProperty(ERROR_PAGE);
-        String login = request.getParameter(LOGIN_PARAMETER);
-        String password = request.getParameter(PASSWORD_PARAMETER);
-        UserValidator validator = new UserValidator();
-        if(validator.checkLogin(login, password)){
+        HttpSession session = request.getSession();
+        User user = (User)session.getAttribute(ATTRIBUTE_USER);
+        String pagePath = null;
+        if(user == null) {
+            pagePath = ConfigurationManager.getProperty(ERROR_PAGE);
+            String login = request.getParameter(LOGIN_PARAMETER);
+            String password = request.getParameter(PASSWORD_PARAMETER);
+            UserValidator validator = new UserValidator();
+            user = validator.checkLogin(login, password);
+        }
+        if(user != null) {
+            session.setAttribute(ATTRIBUTE_USER, user);
             pagePath = ConfigurationManager.getProperty(MAIN_PAGE);
         }
         return pagePath;
--- src/main/java/com/gv/notebook/logic/UserValidator.java
+++ src/main/java/com/gv/notebook/logic/UserValidator.java
@@ -2,30 +2,22 @@ package com.gv.notebook.logic;
 
 import com.gv.notebook.dao.UserDao;
 import com.gv.notebook.dao.UserDaoSimple;
-import com.gv.notebook.models.Note;
 import com.gv.notebook.models.User;
-
 import java.sql.SQLException;
-import java.util.List;
 
 public class UserValidator {
 
-    public boolean checkLogin(String login, String password){
-        boolean isValid = false;
+    public User checkLogin(String login, String password){
+        User user = null;
         UserDao userDao = new UserDaoSimple();
         try {
-            User user = userDao.getUser(login, password);
-            if(user != null){
-                isValid = true;
-                List<Note> notes = user.getAllNotes();
-                // TODO: сохранить в виде глобального объекта (bean?)
-            }
+            user = userDao.getUser(login, password);
         } catch (SQLException e){
             e.printStackTrace();
         } catch (ClassNotFoundException e){
             e.printStackTrace();
         } finally {
-            return isValid;
+            return user;
         }
     }
 }
